c5b98f42906806825eb86c35b39b17ff4c0fe5b9
Kanisorn-S <karamahati@gmail.com>
2025-09-19 08:13:59 +0700

    feat: add mb.py

---
+++ mb.py
@@ -0,0 +1,232 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+import math
+import time
+
+class TurtleShapes(Node):
+    def __init__(self):
+        super().__init__('turtle_shapes_publisher')
+        self.pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.timer_period = 0.1
+        self.twist = Twist()
+
+    def move_circle(self, speed=1.0, radius=0.5, duration=3.14):
+        speed=1.0
+        duration=3.14
+        radius=0.5
+        twist = Twist()
+        twist.linear.x = speed
+        twist.angular.z = speed / radius
+        start_time = time.time()
+        while time.time() - start_time < duration:
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+
+    def move_triangle(self, speed=1.0, side_length=2.0):
+        twist = Twist()
+        # Turn 90 degrees
+        twist.linear.x = 0.0
+        twist.angular.z = math.radians(90)
+        start_time = time.time()
+        while time.time() - start_time < 1.0:
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+        time.sleep(0.5)
+
+        # Move forward
+        twist.linear.x = speed
+        twist.angular.z = 0.0
+        start_time = time.time()
+        while time.time() - start_time < side_length / speed:
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+        time.sleep(0.5)
+
+
+        # Turn -150 degrees
+        twist.linear.x = 0.0
+        twist.angular.z = math.radians(-150)
+        start_time = time.time()
+        while time.time() - start_time < 1.0:
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+        time.sleep(0.5)
+
+
+        # Move forward
+        twist.linear.x = speed
+        twist.angular.z = 0.0
+        start_time = time.time()
+        while time.time() - start_time < side_length / (speed*2):
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+        time.sleep(0.5)
+
+        # Turn 120 degrees
+        twist.linear.x = 0.0
+        twist.angular.z = math.radians(120)
+        start_time = time.time()
+        while time.time() - start_time < 1.0:
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+        time.sleep(0.5)
+
+
+        # Move forward
+        twist.linear.x = speed
+        twist.angular.z = 0.0
+        start_time = time.time()
+        while time.time() - start_time < side_length / (speed*2):
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+        time.sleep(0.5)
+
+
+        # Turn -150 degrees
+        twist.linear.x = 0.0
+        twist.angular.z = math.radians(-150)
+        start_time = time.time()
+        while time.time() - start_time < 1.0:
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+        time.sleep(0.5)
+
+
+
+                # Move forward
+        twist.linear.x = speed
+        twist.angular.z = 0.0
+        start_time = time.time()
+        while time.time() - start_time < side_length / (speed*1):
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+        time.sleep(0.5)
+        # Done M 
+
+        # Turn 90 degrees
+        twist.linear.x = 0.0
+        twist.angular.z = math.radians(90)
+        start_time = time.time()
+        while time.time() - start_time < 1.0:
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+        time.sleep(0.5)
+
+        # Move forward
+        twist.linear.x = speed
+        twist.angular.z = 0.0
+        start_time = time.time()
+        while time.time() - start_time < side_length / (speed*1):
+            self.pub.publish(twist)
+            time.sleep(0.1)
+            
+        self.stop()
+        time.sleep(0.5)
+
+        #Start B
+        #C
+        speed=1.0
+        duration=1.57
+        radius=0.5
+        twist = Twist()
+        twist.linear.x = speed
+        twist.angular.z = speed / radius
+        start_time = time.time()
+        while time.time() - start_time < duration:
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+
+
+        # Turn 180 degrees
+        twist.linear.x = 0.0
+        twist.angular.z = math.radians(180)
+        start_time = time.time()
+        while time.time() - start_time < 1.0:
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+        time.sleep(0.5)
+
+
+
+        #C
+        speed=1.0
+        duration=1.57
+        radius=0.5
+        twist = Twist()
+        twist.linear.x = speed
+        twist.angular.z = speed / radius
+        start_time = time.time()
+        while time.time() - start_time < duration:
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+
+        
+
+        # Turn 90 degrees
+        twist.linear.x = 0.0
+        twist.angular.z = math.radians(90)
+        start_time = time.time()
+        while time.time() - start_time < 1.0:
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+        time.sleep(0.5)
+
+        # Move forward
+        twist.linear.x = speed
+        twist.angular.z = 0.0
+        start_time = time.time()
+        while time.time() - start_time < side_length / (speed*1):
+            self.pub.publish(twist)
+            time.sleep(0.1)
+        self.stop()
+        time.sleep(0.5)
+
+
+        # # Turn -90 degrees
+        # twist.linear.x = 0.0
+        # twist.angular.z = math.radians(-90)
+        # start_time = time.time()
+        # while time.time() - start_time < 1.0:
+        #     self.pub.publish(twist)
+        #     time.sleep(0.1)
+        # self.stop()
+        # time.sleep(0.5)
+
+
+
+
+
+
+
+
+    def stop(self):
+        twist = Twist()
+        self.pub.publish(twist)
+
+def main():
+    rclpy.init()
+    node = TurtleShapes()
+    time.sleep(2)
+    # node.move_circle()
+    node.move_triangle()
+    time.sleep(2)
+    node.stop()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@ WSL -> code . -> Vscode
 
 One wsl terminal open -> ros2 run turtlesim turtlesim_node
 Other one run -> python3 file_name.py
+
+rqt graph -> ros2 run rqt_graph rqt_graph
